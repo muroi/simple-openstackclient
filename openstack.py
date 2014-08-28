678cb18eed53f31272110fba949e01a0a03558fb
Masahito Muroi <muroi.masahito@lab.ntt.co.jp>
2014-08-28 15:23:22 +0900

    add keystone v3 service

--- openstack.py
+++ openstack.py
@@ -14,7 +14,6 @@ class BaseService:
         self.passwd = passwd
         self.tenant = tenant
 
-
 class HttpRequest:
     def __init__(self, url, body={}, headers={}):
         self.url = url
@@ -23,25 +22,32 @@ class HttpRequest:
 
     def call(self, method="GET"):
         if method == "GET":
-            req = requests.get(self.url, headers=self.headers)
+            res = requests.get(self.url, headers=self.headers)
         elif method == "POST":
-            req = requests.post(self.url, data=json.dumps(self.body),
+            res = requests.post(self.url, data=json.dumps(self.body),
                                 headers=self.headers)
         elif method == "DELETE":
-            req = requests.delete(self.url, headers=self.headers)
+            res = requests.delete(self.url, headers=self.headers)
         elif method == "PUT":
-            req = requests.put(self.url, data=json.dumps(self.body),
+            res = requests.put(self.url, data=json.dumps(self.body),
                                headers=self.headers)
+        elif method == "PATCH":
+            res = requests.patch(self.url, data=json.dumps(self.body),
+                               headers=self.headers)
+        elif method == "HEAD":
+            res = requests.head(self.url, headers=self.headers)
         else:
             raise Exception("call not supported method %s" % method)
 
         if debug is True:
-            print 'method' % method
+            print 'method: %s' % method
             print 'url: %s' % self.url
             print 'body: %s' % self.body
-            print 'headers: %s' % self.header
+            print 'headers: %s' % self.headers
+            print 'response headers: %s' % res.headers
+            print 'response body: %s' % res.content
 
-        return req.content
+        return res
 
     
 class HeatService(BaseService):
@@ -62,7 +68,7 @@ class HeatService(BaseService):
             }
 
         request = HttpRequest(url, body, header)
-        resp = json.loads(request.call("POST"))
+        resp = json.loads(request.call("POST").content)
 
         return resp
 
@@ -73,7 +79,7 @@ class HeatService(BaseService):
         url = self._generate_base_url()
 
         request = HttpRequest(url, headers=header)
-        resp = json.loads(request.call())
+        resp = json.loads(request.call().content)
         
         return resp
 
@@ -89,7 +95,7 @@ class HeatService(BaseService):
         url = '/'.join([url, stack_name, stack_id])
 
         request = HttpRequest(url, headers=header)
-        resp = json.loads(request.call())
+        resp = json.loads(request.call().content)
 
         return resp
 
@@ -105,7 +111,7 @@ class HeatService(BaseService):
         url = '/'.join([url, stack_name, stack_id])
 
         request = HttpRequest(url, headers=header)
-        resp = request.call(method='DELETE')
+        resp = request.call(method='DELETE').content
 
         return resp
 
@@ -117,7 +123,7 @@ class HeatService(BaseService):
         url = '/'.join([url, stack_name])
 
         request = HttpRequest(url, headers=header)
-        resp = request.call()
+        resp = request.call().content
 
         return resp
 
@@ -135,7 +141,7 @@ class HeatService(BaseService):
             "template": template,
             }
 
-        request = HttpRequest(url, body, header)
+        request = HttpRequest(url, body, header).content
         resp = request.call("PUT")
 
         return resp
@@ -153,7 +159,7 @@ class HeatService(BaseService):
             }
         
         request = HttpRequest(url, body, header)
-        resp = json.loads(request.call("POST"))
+        resp = json.loads(request.call("POST").content)
 
         return resp
 
@@ -168,9 +174,9 @@ class HeatService(BaseService):
 
         request = HttpRequest(url, headers=header)
         if preview is True:
-            resp = request.call()
+            resp = request.call().content
         else:
-            resp = request.call("DELETE")
+            resp = request.call("DELETE").content
 
         return resp
 
@@ -201,6 +207,448 @@ class HeatService(BaseService):
         if token is not None:
             header['X-Auth-Token'] = token
         
+        return header
+
+class KeystoneService_V3(BaseService):
+    def __init__(self, tenant, name, passwd):
+        BaseService.__init__(self, tenant, name, passwd)
+
+
+    def set_keystone_endpoint(self, url):
+        self.keystone_url = url
+
+
+    def token_get(self):
+        url = self.keystone_url + 'auth/tokens'
+
+        header = {
+            "Content-type": "application/json"
+            }
+
+        data = {
+            "auth": {
+                "identity": {
+                    "methods": [
+                        "password"
+                    ],
+                    "password": {
+                        "user": {
+                            "id"      : self.name,
+                            "password": self.passwd
+                        }
+                    }
+                 }
+             }
+         }
+
+        request = HttpRequest(url, data, header)
+        resp = request.call("POST")
+
+        return resp
+
+
+    def revoke_token(self, token):
+        url = self.keystone_url + 'auth/tokens'
+        header = self._generate_common_headers()
+        header["X-Auth-Token"] = self.generate_token_id()
+        header["X-Subject-Token"] = token
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call("DELETE")
+
+        return resp
+        
+
+    def generate_token_id(self):
+        response = self.token_get()
+        return response.headers["x-subject-token"]
+
+
+    def create_domain(self, name):
+        url = self.keystone_url + 'domains'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-Token"] = self.generate_token_id()
+
+        data = {
+            "domain": {
+                "name": name,
+                "enabled": True,
+                }
+            }
+
+        request = HttpRequest(url, data, header)
+        resp = request.call('POST').content
+
+        return resp
+
+
+    def update_domain(self, domain_id, data):
+        url = self.keystone_url + 'domains/' + domain_id
+        
+        header = self._generate_common_headers()
+        header["X-Auth-Token"] = self.generate_token_id()
+
+        request = HttpRequest(url, data, header)
+        resp = request.call('PATCH').content
+
+        return resp
+
+
+    def list_domain(self):
+        url = self.keystone_url + 'domains'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def delete_domain(self, domain):
+        url = self.keystone_url + 'domains/' + domain
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('DELETE').content
+
+        return resp
+
+
+    def create_project(self, name, domain):
+        url = self.keystone_url + 'projects'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-Token"] = self.generate_token_id()
+        
+        data = {
+            "project": {
+                "name": name,
+                "domain_id": domain,
+                "enabled": True,
+                }
+            }
+
+        request = HttpRequest(url, data, header)
+        resp = request.call('POST').content
+
+        return resp
+
+        
+    def list_project(self):
+        url = self.keystone_url + 'projects'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def delete_project(self, project):
+        url = self.keystone_url + 'projects/' + project
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('DELETE').content
+
+        return resp
+
+
+    def create_user(self, name, password, domain=None):
+        url = self.keystone_url + 'users'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-Token"] = self.generate_token_id()
+        
+        data = {
+            "user": {
+                "name": name,
+                "password": password,
+                "enabled": True,
+                }
+            }
+
+        if domain is not None:
+            data['user']['domain_id'] = domain
+
+        request = HttpRequest(url, data, header)
+        resp = request.call('POST').content
+
+        return resp
+
+
+    def list_user(self):
+        url = self.keystone_url + 'users'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def list_projects_of_user(self, user):
+        url = self.keystone_url + 'users/' + user + '/projects'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def list_roles_of_user(self, user):
+        url = self.keystone_url + 'users/' + user + '/roles'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def delete_user(self, user):
+        url = self.keystone_url + 'users/' + user
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('DELETE').content
+
+        return resp
+
+
+    def create_user_group(self, name, domain):
+        url = self.keystone_url + 'groups'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-Token"] = self.generate_token_id()
+        
+        data = {
+            "group": {
+                "name"      : name,
+                "domain_id" : domain
+                }
+            }
+
+        request = HttpRequest(url, data, header)
+        resp = request.call('POST').content
+
+        return resp
+
+
+    def add_user_to_group(self, group, user):
+        url = self.keystone_url + 'groups/' + group + '/'
+        url = url + 'users/' + user + '/'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('PUT').content
+
+        return resp
+    
+
+    def list_group(self):
+        url = self.keystone_url + 'groups/'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+        
+    def list_users_in_group(self, group):
+        url = self.keystone_url + 'groups/' + group + '/users'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+        
+    def delete_group(self, group):
+        url = self.keystone_url + 'groups/' + group
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('DELETE').content
+
+        return resp
+
+
+    def grant_role_in_project(self, project, role, user):
+        url = self.keystone_url + 'projects/' + project + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/' + role + '/'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('PUT').content
+
+        return resp
+
+
+    def list_roles_for_project(self, project, user):
+        url = self.keystone_url + 'projects/' + project + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def grant_role_in_domain(self, domain, role, user):
+        url = self.keystone_url + 'domains/' + domain + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/' + role + '/'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('PUT').content
+
+        return resp
+
+
+    def list_roles_for_domain(self, domain, user):
+        url = self.keystone_url + 'domains/' + domain + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def list_roles(self):
+        url = self.keystone_url + 'roles'
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def role_assinment(self, user):
+        url = self.keystone_url + 'role_assignments?user.id=' + user
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+        
+
+    def list_inherit_user(self, domain, user):
+        url = self.keystone_url + 'OS-INHERIT/'
+        url = url + 'domains/' + domain + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/'
+        url = url + 'inherited_to_projects'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('GET').content
+
+        return resp
+
+
+    def grant_inherit_role(self, domain, user, role):
+        url = self.keystone_url + 'OS-INHERIT/'
+        url = url + 'domains/' + domain + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/' + role + '/'
+        url = url + 'inherited_to_projects'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('PUT').content
+
+        return resp
+
+
+    def validate_inherit_role(self, domain, user, role):
+        url = self.keystone_url + 'OS-INHERIT/'
+        url = url + 'domains/' + domain + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/' + role + '/'
+        url = url + 'inherited_to_projects'
+        
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('HEAD').content
+
+        return resp
+
+
+    def revoke_inherit_role(self, domain, user, role):
+        url = self.keystone_url + 'OS-INHERIT/'
+        url = url + 'domains/' + domain + '/'
+        url = url + 'users/' + user + '/'
+        url = url + 'roles/' + role + '/'
+        url = url + 'inherited_to_projects'
+
+        header = self._generate_common_headers()
+        header["X-Auth-token"] = self.generate_token_id()
+        
+        request = HttpRequest(url, headers=header)
+        resp = request.call('DELETE').content
+
+        return resp
+
+
+    def _generate_common_headers(self):
+        header = {
+            "Content-type": "application/json"
+            }
+
         return header
 
 class KeystoneService(BaseService):
@@ -251,6 +699,6 @@ class KeystoneService(BaseService):
             }
 
         request = HttpRequest(url, data, header)
-        resp = json.loads(request.call("POST"))
+        resp = request.call("POST").content
 
         return resp
